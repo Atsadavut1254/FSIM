fb8a9707d8f4e1af854c81b1bea9cbb6dc50288d
orbitalno11 <43870591+orbitalno11@users.noreply.github.com>
2020-01-07 14:06:06 +0700

    initial

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import bootstrap set
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import $ from 'jquery'
+import Popper from 'popper.js'
+
+// import route tool
+import { BrowserRouter } from 'react-router-dom'
+
+const AppWithRouter = () => (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
+
+
+ReactDOM.render(<AppWithRouter/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
